fca3cda4ead5c5dd198e0ab4bbf3fb106ec9d964
Pablo Celentano <pablocelentano@gmail.com>
2012-05-03 00:05:35 -0300

    copying stuff from demos

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,41 @@
 package controllers
 
 import play.api._
+import libs.json.JsValue
 import play.api.mvc._
+
 
 object Application extends Controller {
   
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index = Action { implicit request =>
+    Ok(views.html.index())
   }
-  
+
+  /**
+   * Display the main page.
+   */
+
+  def lobby(username: Option[String]) = Action { implicit request =>
+    username.filterNot(_.isEmpty).map { username =>
+      Ok(views.html.battleship(username))
+    }.getOrElse {
+      Redirect(routes.Application.index).flashing(
+        "error" -> "Please choose a valid username."
+      )
+    }
+  }
+
+  /**
+   * Handles the chat websocket.
+   */
+
+  def chat(username: String) = WebSocket.async[JsValue] { request  =>
+
+    ChatRoom.join(username)
+
+  }
+
+
+
+
 }
